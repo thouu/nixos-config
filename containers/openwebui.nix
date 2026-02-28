97be80eab6b5e0e2f7b12aa8eb4dd173321e7108
thouu <nothou@protonmail.com>
2026-02-27 16:31:27 -0800

    multi-host openwebui

--- containers/openwebui.nix
+++ containers/openwebui.nix
@@ -1,24 +1,38 @@
-{ config, ... }:
-{
+{ config, pkgs, ... }:
 
+sops.secrets.openwebui_db_url = {
+  sopsFile = ../home/secrets/secrets.yaml;
+};
+
+let
+  mylar_netbird_ip = "100.126.141.19";
+
+  is_mylar = config.networking.hostName == "mylar";
+
+  db_host = if is_mylar then "postgres" else mylar_netbird_ip;
+  database_url = "postgresql://openwebui:${dbPassword}@${db_host}:5432/openwebui";
+
+  host_port = if is_mylar then "52321" else "52320";
+
+in
+{
   systemd.tmpfiles.rules = [
     "d /home/lcd/containers/openwebui 0755 lcd users -"
   ];
 
   virtualisation.oci-containers.containers.openwebui = {
     image = "ghcr.io/open-webui/open-webui:main";
-    extraOptions = [
-      "--network=homelab"
-    ];
-    ports = [
-      "3000:52320"
-    ];
-    volumes = [
-      "/var/run/docker.sock:/var/run/docker.sock"
-      "/home/lcd/containers/openwebui:/app/backend/data"
+    extraOptions = [ "--network=homelab" ];
+    ports = [ "${host_port}:52320" ];
+    environmentFiles = [
+      config.sops.secrets.openwebui_db_url.path
     ];
     environment = {
       PORT = "52320";
+      DATABASE_URL = database_url;
     };
+    volumes = [
+      "/home/lcd/containers/openwebui:/app/backend/data"
+    ];
   };
 }
