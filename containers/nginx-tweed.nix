fca23bfbaf638d49507f5191c427a08a8ec48ee3
thouu <nothou@protonmail.com>
2026-03-02 19:01:07 -0800

    add proxy headers to qbt.thou.sh

--- containers/nginx-tweed.nix
+++ containers/nginx-tweed.nix
@@ -67,6 +67,11 @@ let
 
         location / {
           proxy_pass http://gluetun:30025;
+          proxy_http_version 1.1;
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
         }
       }
 
