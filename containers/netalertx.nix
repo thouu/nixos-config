03123ccde48c948d0c7bfbe2f2bb1bac4f3fe439
thouu <nothou@protonmail.com>
2026-04-03 18:11:44 -0700

    remove uid & gid from tmpfs (unsupported by podman)

--- containers/netalertx.nix
+++ containers/netalertx.nix
@@ -11,7 +11,7 @@
       "--cap-add=NET_RAW"
       "--cap-add=NET_ADMIN"
       "--cap-add=NET_BIND_SERVICE"
-      "--tmpfs=/tmp:uid=20211,gid=20211,mode=1700"
+      "--tmpfs=/tmp:mode=1700"
     ];
     volumes = [
       "/home/lcd/containers/netalertx/data:/data"
