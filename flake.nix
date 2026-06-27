20007a1ab1477ff38256a0b5650c4c234dd41361
thouu <nothou@protonmail.com>
2026-06-27 09:18:22 -0700

    upgrade from 25.11 to 26.05

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
 
 { description = "nixos multi-host system (tweed + mylar)";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -17,7 +17,7 @@
     unstableOverlay = final: prev:
       let
         pkgsUnstable = import nixpkgs-unstable {
-          system = final.system;
+          system = final.stdenv.hostPlatform.system;
           config.allowUnfree = true;
         };
         unstablePackages = [
